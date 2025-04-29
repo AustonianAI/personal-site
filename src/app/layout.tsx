714f00d8abda57bf4a4f7cae4a9d769e4dc3ccbc
Austin Johnson <Austin@AustinJohnson.me>
2025-04-29 15:31:19 -0500

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Austin Johnson',
+    default: 'Austin Johnson - Software Engineer | Enrolled Agent',
+  },
+  description:
+    'I’m Austin, a software engineer and enrolled agent licensed to practice before the IRS.  Currently residing in Austin, TX, I enjoy solving problems related to tax, software, and finance.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
